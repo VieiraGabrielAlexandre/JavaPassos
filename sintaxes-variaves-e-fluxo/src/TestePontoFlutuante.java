751e173d5acb90f1086b1e6cc071671e51c2e504
Gabriel <gabrielvieira840@gmail.com>
2019-09-03 02:31:50 -0300

    UP Project

---
+++ sintaxes-variaves-e-fluxo/src/TestePontoFlutuante.java
@@ -0,0 +1,7 @@
+
+public class TestePontoFlutuante {
+	public static void main (String[]args) {
+		double salario = 2100.50;
+		System.out.println(salario);
+	}
+}
---
+++ sintaxes-variaves-e-fluxo/src/TesteVariaveis.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+public class TesteVariaveis {
+	
+	public static void main (String[]args) {
+		
+		Scanner ler = new Scanner(System.in);
+		int numero = ler.nextInt();
+		System.out.println("Minha idade é: "+numero);
+	}
+}
